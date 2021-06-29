b0149202924b6f640cc0ae43e82d57ce4040f941
Kamil Janusz <kamiljanusztc@gmail.com>
2021-06-29 17:37:00 +0200

    Add database

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,48 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
+const path = require('path');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+require('dotenv').config();
+// const helmet = require('helmet');
+
+const productsRoutes = require('./routes/products.routes');
 
 const app = express();
-const productsRoutes = require('./routes/products.routes');
-// const productModel = require('./models/product.model');
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../build')));
-
+/* MIDDLEWARE */
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(helmet());
 
+/* API ENDPOINTS */
 app.use('/api', productsRoutes);
 
-app.get('*', (req, res) => {
+/* API ERROR PAGES */
+app.use('/api', (req, res) => {
+  res.status(404).send({ page: 'Not found...' });
+});
+
+/* REACT WEBSITE */
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-app.use((req, res) => {
-  res.status(404).send('404 not found...');
-});
+/* MONGOOSE */
+mongoose.connect(`mongodb+srv://${process.env.userApp}:${process.env.mongoApp}@cluster0.uwrti.mongodb.net/SygnsShop?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect('mongodb+srv://kamil_janusz:-71limak71-@cluster0.uwrti.mongodb.net/SygnsShop?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 const db = mongoose.connection;
-
-app.use(session({
-  secret: '120se',
-  store: new MongoStore({ mongooseConnection: db }),
-}));
-
 db.once('open', () => {
-  console.log('Connected to the database');
+  console.log('Successfully connected to the database');
 });
-db.on('error', err => console.log('Error ' + err));
+db.on('error', err => console.log('Error: ' + err));
 
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
+/* START SERVER */
+console.log('process.env.PORT', process.env.PORT);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log('Server is running on port: '+port);
 });
-
-module.exports = server;
+console.log('port', port);
