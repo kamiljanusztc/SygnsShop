666ce88e13c4501b367cddb6db5ace5417e84364
Kamil Janusz <kamiljanusztc@gmail.com>
2021-06-29 21:34:26 +0200

    Cart problem

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -3,28 +3,6 @@ const router = express.Router();
 
 const Product = require('../models/product.model');
 
-// router.get('/products', async (req, res) => {
-//   try {
-//     const result = await Product
-//       .sort({created: -1});
-//     if(!result) res.status(404).json({ product: 'Not found' });
-//     else res.json(result.map(product => {
-//       console.log('product', product);
-//       return {
-//         _id: product.id,
-//         title: product.title,
-//         content: product.text,
-//         quantity: product.price,
-//         price: product.price,
-//         photo: `http://localhost:8000/${product.photo}`,
-//       };
-//     }));
-//   }
-//   catch(err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 router.get('/products', async (req, res) => {
   try {
     const result = await Product.find();
