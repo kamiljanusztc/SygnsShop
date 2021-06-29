d8d33bf2c8eb4771cfadca6b8c5dcd55ec957a4d
Kamil Janusz <kamiljanusztc@gmail.com>
2021-06-29 20:19:52 +0200

    Change store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,7 @@
-// import {combineReducers, createStore, applyMiddleware} from 'redux';
-// import thunk from 'redux-thunk';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-
-// import { initialState } from './initialState';
-// import { reducer as productsReducer } from './productsRedux';
-// import { reducer as cartReducer } from './cartRedux';
-
-// // define reducers
-// const reducers = {
-//   products: productsReducer,
-//   cart: cartReducer,
-// };
-
-// // add blank reducers for initial state properties without reducers
-// Object.keys(initialState).forEach(item => {
-//   if (typeof reducers[item] == 'undefined') {
-//     reducers[item] = (statePart = null) => statePart;
-//   }
-// });
-
-// const combinedReducers = combineReducers(reducers);
-
-// // create store
-// export const store = createStore(
-//   combinedReducers,
-//   initialState,
-//   composeWithDevTools(
-//     applyMiddleware(thunk)
-//   )
-// );
-
-
 import {combineReducers, createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
 import { initialState } from './initialState';
 import { reducer as productsReducer } from './productsRedux';
